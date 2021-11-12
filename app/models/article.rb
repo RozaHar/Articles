d2c8316fbdf97eac8ab9d0c03a0d3bcbd269f230
RozaHar <rozaharutyuny.129@gmail.com>
2021-11-12 02:37:02 -0800

    Added comments to article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,6 @@ class Article < ApplicationRecord
 
 	has_rich_text :content
 	has_many :article_tags, dependent: :destroy
+	has_many :comments, dependent: :destroy
 	accepts_nested_attributes_for :article_tags
 end
